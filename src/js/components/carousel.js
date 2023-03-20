b84856b3b6afa1e8194aa1fdc140ec5977eb9708
itsmmars <mmars.dev@gmail.com>
2023-03-20 03:18:30 -0700

    SwiperJS implementation issue persists but submitting to meet deadline.

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -1,21 +1,25 @@
 import Swiper from 'swiper';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // eslint-disable-next-line no-new
-  new Swiper('.carousel', {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 1,
+const watch = () => {
+  document.addEventListener('DOMContentLoaded', () => {
+    // eslint-disable-next-line no-new
+    new Swiper('.carousel', {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
-      1024: {
-        slidesPerView: 3,
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
       },
-    },
+    });
   });
-});
+};
+
+watch();
