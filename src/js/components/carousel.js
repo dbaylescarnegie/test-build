6396b665defa869f9998651044328418bdd3f09d
itsmmars <mmars.dev@gmail.com>
2023-03-20 03:11:49 -0700

    styles complete for submission

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -1,21 +1,21 @@
 import Swiper from 'swiper';
 
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  centeredSlides: true,
-  loop: true,
-
-  // If you want pagination, add the following:
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  // If you want navigation buttons, add the following:
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line no-new
+  new Swiper('.carousel', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 // dependencies
+import 'swiper';
 
 // utilities
 import './util/animate';
